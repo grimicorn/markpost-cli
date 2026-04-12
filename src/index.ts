15b49145977385066489f6a69a4442c9b2e7651f
Danny Holloran <dtholloran@gmail.com>
2026-04-12 07:22:38 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+// import { fetchAllRecords } from '@/libs/records.js';
+
+// const test = await fetchAllRecords();
+// console.log(test);
+
 console.log('Hello world!!! - dh-sync-cli');
